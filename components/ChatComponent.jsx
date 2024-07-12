298f67c70f9503d9b4154e9cce5da7717d4926a0
Sankha Bimsara <sankha.b21@gmail.com>
2024-07-12 13:10:32 +0530

    feat: Chat Screen basic functionalities done. Need UX improvements

--- components/ChatComponent.jsx
+++ components/ChatComponent.jsx
@@ -1,29 +1,90 @@
 import { useState } from "react";
-import { View, TextInput, TouchableOpacity } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import { View } from "react-native";
+import ChatPrompInput from "./ChatPromptInput";
+import MessageBubble from "./MessageBubble";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import colors from "../assets/colors";
 
 const ChatComponent = () => {
-  const [prompt, setPrompt] = useState();
+  const [questions, setQuestions] = useState([
+    {
+      id: 1,
+      question: "Tell me something about your skills",
+      answer: "",
+    },
+    {
+      id: 2,
+      question: "What are your career goals?",
+      answer: "",
+    },
+    {
+      id: 3,
+      question: "What are your hobbies?",
+      answer: "",
+    },
+  ]);
+  const [askedQuestions, setAskedQuestions] = useState([1]);
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+
+  const handleSendAnswer = (answer) => {
+    const questionsCopy = [...questions];
+    for (let q of questionsCopy) {
+      if (q.id === currentQuestion) {
+        q.answer = answer;
+      }
+    }
+    setQuestions(questionsCopy);
+    if (currentQuestion === questions.length) {
+      console.log("All questions answered");
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+      setAskedQuestions([...askedQuestions, currentQuestion + 1]);
+    }
+  };
+
   return (
-    <View className={`flex flex-col flex-grow pt-6 pb-4`}>
-      <View className={`flex-grow px-4`}></View>
-      <View className={`px-4 mt-4`}>
-        <View
-          className={`flex flex-row justify-between items-center border border-black-200 rounded-lg px-4 py-2`}
-        >
-          <TextInput
-            className={`mr-8 flex-grow`}
-            placeholder="Type a message"
-            multiline={true}
-            value={prompt}
-            onChangeText={(text) => setPrompt(text)}
-          />
-          <TouchableOpacity style={{ position: "absolute", right: 10 }}>
-            <Ionicons name="send" size={24} color={colors.secondary} />
-          </TouchableOpacity>
-        </View>
+    <View className={`flex flex-col flex-grow pt-6 pb-4 bg-white`}>
+      <View className={`flex-grow px-4 justify-end`}>
+        {askedQuestions.map((qId) => {
+          const q = questions.find((q) => q.id === qId);
+          return (
+            <View key={q.id} className={`flex flex-col`}>
+              <View className={`flex flex-row items-start mr-16 my-2`}>
+                <MaterialCommunityIcons
+                  name="google-assistant"
+                  size={24}
+                  color={colors.secondary}
+                />
+                <MessageBubble
+                  message={q.question}
+                  color="secondary"
+                  textColor="white"
+                  className="ml-2"
+                />
+              </View>
+              {q.answer != "" && (
+                <View
+                  className={`w-full flex flex-row items-start justify-end pl-16 my-2`}
+                >
+                  <MessageBubble
+                    message={q.answer}
+                    color="black-100"
+                    textColor="black"
+                    className="mr-2"
+                  />
+                  <FontAwesome name="user" size={24} color={colors.secondary} />
+                </View>
+              )}
+            </View>
+          );
+        })}
       </View>
+      <ChatPrompInput
+        onSend={(answer) => {
+          handleSendAnswer(answer);
+        }}
+      />
     </View>
   );
 };
